114d35ac8454046edf9daa7a5b5bd11f63fdd1a9
Jordan Heatwole <36244068+jordie324@users.noreply.github.com>
2020-09-27 16:49:59 -0500

    Add files via upload

---
+++ topNavMobile/topNavMobile.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+
+
+    $(document).ready(function () {
+        $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            $(this).parent().siblings().removeClass('open');
+            $(this).parent().toggleClass('open');
+        });
+    });
+
+});
